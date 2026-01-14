5d1ea2f0da1c77543fcdddf9f6789c5bb75ab329
Al Duane C. Mirasol <alduane.mirasol@ctug.edu>
2026-01-14 10:39:33 +0800

    updated

--- lib/providers/global_providers.dart
+++ lib/providers/global_providers.dart
@@ -2,10 +2,14 @@ import '../repositories/account_repository.dart';
 import '../services/db_service.dart';
 
 class GlobalProviders {
+  // Global instances
   static late final AccountRepository accountRepository;
 
   static Future<void> init() async {
+    // Initialize db
     final db = await DBService().database;
+
+    // Initialize repositories
     accountRepository = AccountRepository(db);
   }
 }
