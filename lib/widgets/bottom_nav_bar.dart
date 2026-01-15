fad10fa7cac69f99226dbd5ca7946468036b5e08
alduanemirasol <alduanecuevasmirasol@gmail.com>
2026-01-15 20:45:22 +0800

    updated

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -10,8 +10,21 @@ const List<NavigationItem> navItems = [
   NavigationItem(icon: Icons.settings, label: 'Settings', route: '/settings'),
 ];
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  // Build navigation bar items
+  static final List<BottomNavigationBarItem> _navBarItems = navItems
+      .map(
+        (item) =>
+            BottomNavigationBarItem(icon: Icon(item.icon), label: item.label),
+      )
+      .toList();
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +32,12 @@ class BottomNavBar extends StatelessWidget {
 
     return BottomNavigationBar(
       currentIndex: currentIndex,
-      onTap: (index) => context.go(navItems[index].route),
-      items: _buildNavItems(),
+      onTap: (index) {
+        if (currentIndex != index) {
+          context.go(navItems[index].route);
+        }
+      },
+      items: _navBarItems,
       type: BottomNavigationBarType.fixed,
       selectedItemColor: AppColors.primary,
       unselectedItemColor: Colors.grey,
@@ -33,14 +50,4 @@ int _resolveCurrentIndex(BuildContext context) {
   final location = GoRouterState.of(context).uri.path;
   final index = navItems.indexWhere((item) => location.startsWith(item.route));
   return index == -1 ? 0 : index;
-}
-
-// Build bottom navigation bar items
-List<BottomNavigationBarItem> _buildNavItems() {
-  return navItems
-      .map(
-        (item) =>
-            BottomNavigationBarItem(icon: Icon(item.icon), label: item.label),
-      )
-      .toList();
 }
