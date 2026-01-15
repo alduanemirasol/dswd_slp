f6489b51b90cf179710f5ac5dcfb951514a82996
alduanemirasol <alduanecuevasmirasol@gmail.com>
2026-01-15 20:38:28 +0800

    updated

--- lib/models/navigation_item_model.dart
+++ lib/models/navigation_item_model.dart
@@ -4,12 +4,10 @@ class NavigationItem {
   final IconData icon;
   final String label;
   final String route;
-  final Widget page;
 
   const NavigationItem({
     required this.icon,
     required this.label,
     required this.route,
-    required this.page,
   });
 }
--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,61 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../models/navigation_item_model.dart';
-import '../views/history_view.dart';
-import '../views/home_view.dart';
-import '../views/settings_view.dart';
+import '../core/colors.dart';
+
+// Navigation items
+const List<NavigationItem> navItems = [
+  NavigationItem(icon: Icons.home, label: 'Home', route: '/home'),
+  NavigationItem(icon: Icons.history, label: 'History', route: '/history'),
+  NavigationItem(icon: Icons.settings, label: 'Settings', route: '/settings'),
+];
 
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key});
 
-  static const List<NavigationItem> navigationItems = [
-    NavigationItem(
-      icon: Icons.home,
-      label: 'Home',
-      route: '/home',
-      page: HomeView(),
-    ),
-    NavigationItem(
-      icon: Icons.history,
-      label: 'History',
-      route: '/history',
-      page: HistoryView(),
-    ),
-    NavigationItem(
-      icon: Icons.settings,
-      label: 'Settings',
-      route: '/settings',
-      page: SettingsView(),
-    ),
-  ];
-
-  // Get current index from route
-  int _currentIndex(BuildContext context) {
-    final location = GoRouterState.of(context).uri.path;
-    final index = navigationItems.indexWhere(
-      (item) => location.startsWith(item.route),
-    );
-    return index < 0 ? 0 : index;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final currentIndex = _currentIndex(context);
+    final currentIndex = _resolveCurrentIndex(context);
 
     return BottomNavigationBar(
       currentIndex: currentIndex,
-      onTap: (index) => context.go(navigationItems[index].route),
-      items: navigationItems
-          .map(
-            (item) => BottomNavigationBarItem(
-              icon: Icon(item.icon),
-              label: item.label,
-            ),
-          )
-          .toList(),
+      onTap: (index) => context.go(navItems[index].route),
+      items: _buildNavItems(),
       type: BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.blue,
+      selectedItemColor: AppColors.primary,
       unselectedItemColor: Colors.grey,
     );
   }
+}
+
+// Get current index from route
+int _resolveCurrentIndex(BuildContext context) {
+  final location = GoRouterState.of(context).uri.path;
+  final index = navItems.indexWhere((item) => location.startsWith(item.route));
+  return index == -1 ? 0 : index;
+}
+
+// Build bottom navigation bar items
+List<BottomNavigationBarItem> _buildNavItems() {
+  return navItems
+      .map(
+        (item) =>
+            BottomNavigationBarItem(icon: Icon(item.icon), label: item.label),
+      )
+      .toList();
 }
